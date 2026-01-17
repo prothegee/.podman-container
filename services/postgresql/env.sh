4a94fea9a336f1ad2a82623ac426a2ee52e2f8e0
prothegee <prothegee@proton.me>
2026-01-18 02:10:04 +0700

    matching container name

--- services/postgresql/env.sh
+++ services/postgresql/env.sh
@@ -1,3 +1,6 @@
-_pg_psql() {
+_pg_psql_1_cr() {
 	podman exec -it postgres-1-cr /bin/bash -c "psql -U postgres";
+}
+_pg_psql_2_hw() {
+	podman exec -it postgres-2-hw /bin/bash -c "psql -U postgres";
 }
--- services/redis/env.sh
+++ services/redis/env.sh
@@ -2,5 +2,5 @@ _redis_cli() {
     echo -n "redis password: ";
     read -s REDIS_PASS;
     echo;
-    docker exec -it redis redis-cli -a "$REDIS_PASS";# --no-auth-warning;
+    podman exec -it redis-1-cc redis-cli -a "$REDIS_PASS";# --no-auth-warning;
 }
