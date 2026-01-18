aeb34ec8905513941e2491f7003b0636879fc3bb
prothegee <prothegee@proton.me>
2026-01-18 14:57:21 +0700

    adding up container script

---
+++ dup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+CURRENT_DIR=$(pwd);
+
+data_dirs=(
+    "$(pwd)/services/kafka/data-kafka-1"
+    "$(pwd)/services/postgresql/data-postgres-1-cr"
+    "$(pwd)/services/postgresql/data-postgres-2-hw"
+    "$(pwd)/services/rabbitmq/data-rabbitmq-1"
+    "$(pwd)/services/redis/data-redis-1-cc"
+    "$(pwd)/services/scylladb/data-scylladb-1"
+)
+for dir in "${data_dirs[@]}"; do
+    mkdir -p "$dir";
+done
+
+services_dirs=(
+    "$(pwd)/services/kafka"
+    "$(pwd)/services/postgresql"
+    "$(pwd)/services/rabbitmq"
+    "$(pwd)/services/redis"
+    "$(pwd)/services/scylladb"
+)
+for dir in "${services_dirs[@]}"; do
+    cd $dir;
+    podman-compose -f ./podman.yaml up -d;
+    cd ..;
+done
